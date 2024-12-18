2f68012448c5fc82ffd23acc79ccdb485db0a7c4
Jacky <jacky@iyes.dev>
2024-12-18 21:26:57 +0800

    update: client package add prettier

--- packages/client/app/page.tsx
+++ packages/client/app/page.tsx
@@ -1,25 +1,25 @@
-"use client";
-import Image from "next/image";
-import { useEffect } from "react";
-import { Api } from "@/__generated__/api-types";
-import { greeting } from "shared";
+'use client'
+import Image from 'next/image'
+import { useEffect } from 'react'
+import { Api } from '@/__generated__/api-types'
+import { greeting } from 'shared'
 
-greeting();
+greeting()
 
 const api = new Api({
-  baseUrl: process.env.NEXT_PUBLIC_API_URL
-});
+  baseUrl: process.env.NEXT_PUBLIC_API_URL,
+})
 
 const requestGetHello = async () => {
-  const response = await api.appControllerGetHello();
-  const res = await response.json();
-  return res;
+  const response = await api.appControllerGetHello()
+  const res = await response.json()
+  return res
 }
 
 export default function Home() {
   useEffect(() => {
-    requestGetHello();
-  }, []);
+    requestGetHello()
+  }, [])
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -33,7 +33,7 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            Get started by editing{" "}
+            Get started by editing{' '}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               app/page.tsx
             </code>
@@ -116,5 +116,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  );
+  )
 }
